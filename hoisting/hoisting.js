9f5268794c4845e3ef85edfe91f0e15f3b2304f1
Jaikisha Gupta <jaikishaupta754@gmail.com>
2024-07-28 12:36:45 +0530

    hositing

---
+++ hoisting/hoisting.js
@@ -0,0 +1,2 @@
+// console.log(a);
+// let a = 10; //Cannot access 'a' before initialization
--- test.js
+++ test.js
@@ -1,2 +1,28 @@
-const hex = "0123456789ABCDEF";
-console.log(hex[15]);
+// const hex = "0123456789ABCDEF";
+// console.log(hex[15]);
+
+// name = "moto";
+// let name;
+// console.log(name);  //Cannot access 'name' before initialization
+
+// name = "moto";
+// var name;
+// console.log(name); //moto
+
+// a = 10;
+// console.log(a); //10
+
+// "use strict";
+// b = 20;
+// console.log(b); //after using use strict we get error -> b is not defined
+
+// var a = 10;
+// console.log(a);
+// delete a;
+// console.log(a);  //10
+
+// "use strict";
+// var a = 10;
+// console.log(a);
+// delete a; //Delete of an unqualified identifier in strict mode.
+// console.log(a);
